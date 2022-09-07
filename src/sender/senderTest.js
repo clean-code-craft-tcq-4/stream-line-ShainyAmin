a99a3c144bf89d0bb9e733a90d623ebd1f386fc5
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-09-07 20:20:43 +0530

    Add files via upload

--- src/sender/senderTest.js
+++ src/sender/senderTest.js
@@ -1,11 +1,29 @@
 const sensorReadings = require('./sensorReadings');
-const senderOutput = require('./sender');
+
 
 const {expect} = require('chai');
 
-it('Test sender output in console', () => {
-    console.log = jest.fn();
-    log(sensorReadings);
-    // The first argument of the first call to the function was 'hello'
-    expect(console.log.mock.calls[0][0]).toBe(sensorReadings);
+describe('Test sensor readings random generator', () => {
+  let readings = sensorReadings.fetchReadings();
+  it('should exists', () => {
+      expect(sensorReadings.fetchReadings).to.exist;
   });
+  it('should return sensor value', () => {
+
+      expect(readings.length).to.deep.equals(50);
+  });
+});
+
+describe(' sensor temperature should exists', () => {
+  it('should exists', () => {
+      expect(sensorReadings.senderTemperatureReadings).to.exist;
+  });
+
+});
+
+describe('sensor SOC should exists', () => {
+  it('should exists', () => {
+      expect(sensorReadings.senderSOCReadings).to.exist;
+  });
+
+});
